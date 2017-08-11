114c9a72f36da3ac57d476cc998c352318b69b8b
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-10 22:00:54 -0400

    f/ exc 0 through 1

---
+++ exc0.rb
@@ -0,0 +1,50 @@
+# 0. array creation
+
+fav_colors = ["green", "purple", "black"]
+
+ages = [7, 8, 25]
+
+coin_flip = ["no", "yes", "yes"]
+
+artists = ["tears for fears", "duran duran" "billy joel"]
+
+favor_colors = [:green, :purple, :black]
+
+definitions = {
+  :tank => "something metal with wheels",
+  :car => "a smaller tank",
+  :bike => "a very small tank"
+}
+
+fav_movies = {
+  :it => 1984,
+  :usuaulsuspects => 1985,
+  :jamesbond => 1986
+}
+
+cities = {
+  :roma => 100,
+  :toronto => 1000,
+  :paris => 10000
+}
+
+sibs = {
+  :cris => 27,
+  :joe => 25,
+  :carmine => 55,
+  :josie => 60
+}
+
+# 1.
+
+puts coin_flip
+
+puts fav_colors[0]
+
+p ages.sort
+
+p ages[3] = 0
+
+p ages.sort
+
+p fav_movies[:it]
